3d62194e310a26a409f0ce46ee577904e84a216a
Aika1988 <dzhumagulovaaika88@icloud.com>
2023-03-09 14:30:40 +0600

    sss

--- account/send_email.py
+++ account/send_email.py
@@ -3,14 +3,13 @@ from django.core.mail import send_mail
 def send_activation_code(email, code):
     send_mail(
     'Py25 shop project', # title
-    f'http://localhost:8000/api/v1/account/activate/{code}', # body
+    f'http://35.246.178.169/api/v1/account/activate/{code}', # body
     'ajkanysdzumagulova@gmail.com',
     [email]
     ) # to 
 
 
 def send_reset_password_code(email, code):
-
     send_mail(
     'Py25 shop project', # title
     f'Привет чтобы сбросить пароль тебе нужно знать этот токен = {code}', # body
